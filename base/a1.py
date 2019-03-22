860107ca1e8c02ad8bcdee0946429c6d3961b0f8
liuwei <levonfly@gmail.com>
2019-03-22 16:08:01 +0800

    first commit

---
+++ base/a1.py
@@ -0,0 +1,9 @@
+from urllib import request
+
+
+with request.urlopen('https://cuiqingcai.com/category/resources') as f:
+    data = f.read()
+    print('Status:', f.status, f.reason)
+    for k, v in f.getheaders():
+        print('%s: %s' % (k, v))
+    print('Data:', data.decode('utf-8'))
